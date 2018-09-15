eb97699ba1e2245b738d986886761ff75b2aef7f
yohanis-ke <yohanes.ke@gmail.com>
2018-09-15 11:42:07 -0400

    The instance of trip is created

--- location-oop.rb
+++ location-oop.rb
@@ -17,4 +17,21 @@ class Trip < Location
     @locations << location
     return location
   end
+
+  def travels
+    puts 'Began trip'
+    @locations.each_cons(2) do |location|
+      puts "Travelled from #{location[0].name} to #{location[1].name}"
+
+    end
+    puts 'Ended trip'
+  end
 end
+
+trip1= Trip.new
+trip1.add('Ottawa')
+trip1.add('Montreal')
+trip1.add('Quebec')
+trip1.add('Alberta')
+
+trip1.travels
